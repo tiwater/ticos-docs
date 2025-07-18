f51bd032ce9423dda952c84c1b2ae555ef670a4c
Sawyer <35255629+shaoyie@users.noreply.github.com>
2025-07-18 17:14:09 +0800

    docs: update the title

--- content/en/docs/api/_meta.ts
+++ content/en/docs/api/_meta.ts
@@ -3,5 +3,5 @@ import type { Meta } from "nextra";
 export default {
   "index": "Overview",
   "account-level": "Account Level APIs",
-  'terminal-level': 'Terminal-Level API',
+  "terminal-level": "Terminal Level APIs"
 } satisfies Meta;
--- content/zh/docs/api/_meta.ts
+++ content/zh/docs/api/_meta.ts
@@ -3,5 +3,5 @@ import type { Meta } from "nextra";
 export default {
   "index": "总览",
   "account-level": "账户级 API",
-  'terminal-level': '终端级接口',
+  "terminal-level": "终端级 API"
 } satisfies Meta;
